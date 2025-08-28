a5a4afcd805c18978e7c48188c5baef34f71d220
Meet Bhadra <meetbhadra676@gmail.com>
2025-08-28 10:08:52 +0530

    initiate commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import  cors from 'cors';
 
 const app = express();
 app.use(cors());
@@ -15,6 +15,7 @@ const products = [
 ];
 
 app.get('/api/products', (req, res) => {
+    console.log("hi")
   res.json(products);
 });
 
